b53e630df63115a04f235912124a1c6ab9485302
ozheng7 <ozheng@ucsd.edu>
2022-05-01 20:55:55 -0700

    bringing back the error

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,7 +3,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void togetherTest() {
-        assertEquals("hi person", SkillDemo.together("hi", " person")); // test that fails because the arguments have a space while the expected is spaceless
+        assertEquals("hiperson", SkillDemo.together("hi", " person")); // test that fails because the arguments have a space while the expected is spaceless
         // nwo the test works because the space was added
     }
 }
