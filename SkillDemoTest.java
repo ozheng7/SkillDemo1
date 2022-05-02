b86dce273079c46c62ab6f2eeb8218eefc8cadfa
ozheng7 <103291913+ozheng7@users.noreply.github.com>
2022-05-01 20:46:44 -0700

    Create SkillDemoTest.java

---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void togetherTest() {
+        assertEquals("hiperson", SkillDemo.together("hi", " person")); // test that fails because the arguments have a space while the expected is spaceless
+    }
+}
